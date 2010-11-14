815d20383a5372031e99d47b0cdb2503b14e8f55
halsafar <halsafar@hal-vm-ubuntu>
2010-11-14 00:46:30 -0600

    - compiles fine now - does nothing yet but ready to setup

--- trunk/src/VbaPs3.cpp
+++ trunk/src/VbaPs3.cpp
@@ -4,11 +4,32 @@
  *  Created on: Nov 13, 2010
  *      Author: halsafar
  */
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/timer.h>
+#include <sys/return_code.h>
+#include <sys/process.h>
+#include <sys/spu_initialize.h>
+#include <sysutil/sysutil_sysparam.h>
+
+#include <cell/sysmodule.h>
+#include <cell/cell_fs.h>
+#include <cell/pad.h>
+
+SYS_PROCESS_PARAM(1001, 0x10000);
+
 
 int emulating = 0;
 
 
 int main()
 {
+	// Initialize 6 SPUs but reserve 1 SPU as a raw SPU for PSGL
+	sys_spu_initialize(6, 1);
 
+	cellSysmoduleLoadModule(CELL_SYSMODULE_FS);
+	cellSysmoduleLoadModule(CELL_SYSMODULE_IO);
 }
