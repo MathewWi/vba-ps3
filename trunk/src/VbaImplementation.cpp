af606bad68dc698abb24ed2da8cb95847050b9c7
halsafar <halsafar@hal-vm-ubuntu>
2010-11-16 15:47:14 -0600

    - updated VbaImplementation a bit with some more VBA required functions - roms load, emulation loops works, can exit with our standard input - no graphics, audio, input done yet for vba

--- trunk/src/VbaImplementation.cpp
+++ trunk/src/VbaImplementation.cpp
@@ -11,78 +11,97 @@
 #include "vba/common/SoundDriver.h"
 
 #include "cellframework/logger/Logger.h"
+#include "cellframework/utility/utils.h"
 
+// VBA - must define these
 int RGB_LOW_BITS_MASK = 0;
 
 u16 systemColorMap16[0x10000];
 u32 systemColorMap32[0x10000];
 u16 systemGbPalette[24];
-int systemRedShift;
-int systemGreenShift;
-int systemBlueShift;
-int systemColorDepth;
-int systemDebug;
-int systemVerbose;
-int systemFrameSkip;
-int systemSaveUpdateCounter;
-int systemSpeed;
+int systemRedShift = 0;
+int systemBlueShift = 0;
+int systemGreenShift = 0;
+int systemColorDepth = 16;
+int systemDebug = 0;
+int systemVerbose = 1;
+int systemFrameSkip = 0;
+int systemSaveUpdateCounter = SYSTEM_SAVE_NOT_UPDATED;
+int systemSpeed = 0;
 
 void (*dbgOutput)(const char *s, u32 addr);
 void (*dbgSignal)(int sig,int number);
 
+// start time of the system
+uint32_t startTime = 0;
 
-extern void log(const char *,...)
+extern void log(const char * fmt,...)
+{
+	LOG("VbaLog:\n\t");
+
+	va_list args;
+	va_start(args,fmt);
+	LOG(fmt, args);
+	va_end(args);
+
+	LOG("\n");
+}
+
+
+// shinhalsafar: Vba should force this... there is some setup.
+bool systemInit()
+{
+	startTime = sys_time_get_system_time();
+}
+
+
+bool systemPauseOnFrame()
+{
+	return false;
+}
+
+
+void systemGbPrint(uint8_t *,int,int,int,int)
 {
 
 }
 
 
-extern bool systemPauseOnFrame()
+void systemScreenCapture(int)
 {
 
 }
 
 
-extern void systemGbPrint(uint8_t *,int,int,int,int)
-{
-
-}
-
-
-extern void systemScreenCapture(int)
-{
-
-}
-
-
-extern void systemDrawScreen()
+void systemDrawScreen()
 {
 
 }
 
 
 // updates the joystick data
-extern bool systemReadJoypads()
+bool systemReadJoypads()
 {
 
 }
 
 
 // return information about the given joystick, -1 for default joystick
-extern uint32_t systemReadJoypad(int)
+uint32_t systemReadJoypad(int)
 {
 
 }
 
 
-extern uint32_t systemGetClock()
+uint32_t systemGetClock()
 {
-    //uint32_t now = gettime();
+    uint32_t now = sys_time_get_system_time();
+    return now - startTime;
     //return diff_usec(start, now) / 1000;
 }
 
 
-extern void systemMessage(int id, const char * fmt, ...)
+void systemMessage(int id, const char * fmt, ...)
 {
 	LOG("systemMessage(%d)\n\t", id);
 
@@ -95,76 +114,109 @@ extern void systemMessage(int id, const char * fmt, ...)
 }
 
 
-extern void systemSetTitle(const char *)
-{
-
-}
-extern SoundDriver* systemSoundInit()
-{
-
-}
-extern void systemOnWriteDataToSoundBuffer(const uint16_t * finalWave, int length)
-{
-
-}
-extern void systemOnSoundShutdown()
-{
-
-}
-extern void systemScreenMessage(const char *)
-{
-
-}
-extern void systemUpdateMotionSensor()
-{
-
-}
-extern int  systemGetSensorX()
-{
-
-}
-extern int  systemGetSensorY()
-{
-
-}
-extern bool systemCanChangeSoundQuality()
-{
-
-}
-extern void systemShowSpeed(int)
-{
-
-}
-extern void system10Frames(int)
-{
-
-}
-extern void systemFrame()
-{
-
-}
-extern void systemGbBorderOn()
+void systemSetTitle(const char *)
 {
 
 }
 
-extern void Sm60FPS_Init()
+
+SoundDriver* systemSoundInit()
+{
+	return NULL;
+}
+
+
+void systemOnWriteDataToSoundBuffer(const uint16_t * finalWave, int length)
 {
 
 }
-extern bool Sm60FPS_CanSkipFrame()
+
+
+void systemOnSoundShutdown()
 {
 
 }
-extern void Sm60FPS_Sleep()
+
+
+void systemScreenMessage(const char *)
 {
 
 }
-extern void DbgMsg(const char *msg, ...)
+
+
+void systemUpdateMotionSensor()
 {
 
 }
-extern void winlog(const char *,...)
+
+
+int  systemGetSensorX()
+{
+	return 0;
+}
+
+
+int  systemGetSensorY()
+{
+	return 0;
+}
+
+
+bool systemCanChangeSoundQuality()
+{
+	return false;
+}
+
+
+void systemShowSpeed(int)
+{
+
+}
+
+
+void system10Frames(int)
+{
+
+}
+
+
+ void systemFrame()
+{
+
+}
+
+
+void systemGbBorderOn()
+{
+
+}
+
+
+ void Sm60FPS_Init()
+{
+
+}
+
+
+bool Sm60FPS_CanSkipFrame()
+{
+
+}
+
+
+void Sm60FPS_Sleep()
+{
+
+}
+
+
+void DbgMsg(const char *msg, ...)
+{
+
+}
+
+
+void winlog(const char *,...)
 {
 
 }
