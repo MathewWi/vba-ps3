d667b4938a40a7085621be4d616dc6d47c72e155
halsafar <halsafar@hal-vm-ubuntu>
2010-11-25 00:51:43 -0600

    - added zip io

---
+++ trunk/src/VbaZipIo.h
@@ -0,0 +1,71 @@
+/*
+ * VbaZipIo.h
+ *
+ *  Created on: Nov 24, 2010
+ *      Author: halsafar
+ */
+
+#ifndef VBAZIPIO_H_
+#define VBAZIPIO_H_
+
+#include <stack>
+#include <string>
+#include <map>
+#include <vector>
+
+#include "utils/fex/Zip7_Extractor.h"
+
+#define ZIPIO_TYPE_DIR 0
+#define ZIPIO_TYPE_FILE 1
+
+struct ZipEntry
+{
+	std::string name;
+	int pos;
+	int type;
+};
+
+class VbaZipIo
+{
+public:
+	VbaZipIo();
+	~VbaZipIo();
+
+	void Open(std::string filename);
+	void PushDir(std::string dir);
+	void PopDir();
+	size_t GetCurrentEntryCount();
+	void SetCurrentEntryPosition(size_t index);
+
+	uint8_t* GetEntryData();
+
+	ZipEntry GetCurrentEntry()
+	{
+		return _zipMap[_dir.top()][_currentDirIndex];
+	}
+
+	size_t GetCurrentEntryIndex()
+	{
+		return _currentDirIndex;
+	}
+
+	int GetCurrentEntrySize();
+
+	size_t GetDirStackCount()
+	{
+		return _dir.size();
+	}
+
+	ZipEntry operator[](uint32_t i)
+	{
+		return _zipMap[_dir.top()][i];
+	}
+private:
+	Zip7_Extractor _zip7;
+
+	std::stack<std::string> _dir;
+	std::size_t _currentDirIndex;
+	std::map<std::string, std::vector<struct ZipEntry> > _zipMap;
+};
+
+#endif /* VBAZIPIO_H_ */
